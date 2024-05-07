001de19057af22750d49b933945aa901d51e8db0
Radwanbaba13 <radwangbaba@gmail.com>
2024-05-06 22:45:48 -0400

    Added Individual Pages

--- ui-task/src/App.js
+++ ui-task/src/App.js
@@ -1,12 +1,21 @@
-import logo from "./Static/logo512.png";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage.tsx";
+import HomePage from "./Pages/HomePage.tsx";
+import IntrafleetPage from "./Pages/IntrafleetPage.tsx";
+import NotFoundPage from "./Pages/NotFoundPage.tsx";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/intrafleet" element={<IntrafleetPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ ui-task/src/Pages/IntrafleetPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IntrafleetPage = () => {
+  return (
+    <div>
+      <h1>Intrafleet Page</h1>
+    </div>
+  );
+};
+
+export default IntrafleetPage;
